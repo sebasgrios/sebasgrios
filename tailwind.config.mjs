ec347e8b1bebbee124cb87bb765975325ac6aebe
sgonzari <sebastiangrios08@gmail.com>
2024-10-06 20:05:43 +0200

    ✨ Add version 2.0

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'selector',
 	theme: {
-		extend: {
-			backgroundColor: {
-				'light': '#F9F9FB',
-				'dark': '#060604',
-			},
-			colors: {
-				'primary': '',
-				'secondary': ''
-			}
-		},
+		extend: {},
 	},
 	plugins: [],
 }
