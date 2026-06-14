a2e4096e96650d0902023863b1f061a8e087f9f7
sebasgrios <45594459+sebasgrios@users.noreply.github.com>
2026-06-14 17:43:43 +0200

    🧽 start new portfolio

--- src/config/site.ts
+++
@@ -1,8 +0,0 @@
-export const SITE = {
-  url: 'https://sebasgrios.es',
-  domain: 'sebasgrios.es',
-  name: 'Sebastián González Ríos',
-  defaultEmail: 'contact@sebasgrios.es',
-  defaultLinkedin: 'https://linkedin.com/in/sebasgrios/',
-  defaultGithub: 'https://github.com/sebasgrios',
-} as const;
